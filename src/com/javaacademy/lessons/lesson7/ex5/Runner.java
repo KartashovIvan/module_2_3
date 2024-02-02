b5c7181e1c36ba393a03bce4475fd2e04ddf9921
YMolodyko <VTB4081847@region.vtb.ru>
2024-02-02 09:29:00 +0300

    lesson8

--- src/com/javaacademy/lessons/lesson7/ex5/Runner.java
+++ src/com/javaacademy/lessons/lesson7/ex5/Runner.java
@@ -6,9 +6,9 @@ import java.util.stream.Stream;
 
 public class Runner {
     public static void main(String[] args) {
-        //Терминальные операции
+        //Терминальные операции (Заканчивают работу со стримом)
         Stream<String> stream = Stream.of("A", "B", "C");
-        //for each
+        //Действие с каждым элементом
         stream.forEach(el -> System.out.println(el));
 
         //Возвращает количество элементов стрима
@@ -17,19 +17,24 @@ public class Runner {
 
         //Логические операции
         //Хотя бы один элемент соответствует условию
-        Stream.of(null, "B", "C").anyMatch(element -> "B".equals(element));
-        boolean isElementsLength1 = Stream.of("A", "B", "C")
-                .allMatch(element -> element.length() == 1);
+        boolean anyMatch = Stream.of(null, "B", "C").anyMatch(element -> "B".equals(element));
+
+        //Все элементы соответствует условию
+        boolean allMatch = Stream.of("A", "B", "C").allMatch(element -> element.length() == 1);
+
+        //Ни один элемент не соответствует условию
         boolean noneMatch = Stream.of("A", "B", "C").noneMatch(el -> el == null);
-        System.out.println(noneMatch);
+
 
         //Возвращение одного элемента
+
         //Возвращение первого элемента
         String text = Stream.of("Бабушка1", "Бабушка2", "Бабушка3")
                 .findFirst()
                 .orElse("Заглушка");
         System.out.println(text);
 
+        //Возвращение любого элемента
         String text2 = Stream.of("Бабушка1", "Бабушка2", "Бабушка3")
                 .findAny()
                 .orElse("Заглушка");
@@ -47,7 +52,7 @@ public class Runner {
                 .orElseThrow();
         System.out.println(maxValue);
 
-        //Собрать все воедино
+        //Собрать все в одно значение
         Integer summ = Stream.of(50, 100, 200).reduce(1000, (a, b) -> a + b);
         System.out.println(summ);
 
